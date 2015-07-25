e1cb0facdcfb9dfa52d10f79f20f4c55c3d8259e
AHN <eu81273@gmail.com>
2015-07-26 01:23:52 +0900

    first commit :)

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyhtml = require('gulp-minify-html');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+
+//자바스크립트 파일을 하나로 합치고 압축한다.
+gulp.task('optimize-js', function () {
+	return gulp.src(['src/js/!(main).js', 'src/js/main.js'])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/images'));
+});
+
+//CSS 파일을 하나로 합치고 압축한다.
+gulp.task('optimize-css', function () {
+	return gulp.src(['src/css/!(style).css', 'src/css/style.css'])
+		.pipe(concat('style.css'))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/'));
+});
+
+//HTML 파일을 압축한다.
+gulp.task('optimize-html', function () {
+	return gulp.src('src/skin.html')
+		.pipe(minifyhtml({comments:true}))
+		.pipe(gulp.dest('dist/'));
+});
+
+//index.xml 파일을 복사한다.
+gulp.task('serve-xml', function () {
+	return gulp.src('src/index.xml')
+		.pipe(gulp.dest('dist/'));
+});
+
+// 파일 변경 감지
+gulp.task('watch', function () {
+	gulp.watch('src/js/*.js', ['optimize-js']);
+	gulp.watch('src/css/*.css', ['optimize-css']);
+	gulp.watch('src/skin.html', ['optimize-html']);
+	gulp.watch('src/index.xml', ['serve-xml']);
+});
+
+//기본 task 설정
+gulp.task('default', ['optimize-js', 'optimize-css','optimize-html', 'optimize-html', 'serve-xml', 'watch' ]);
---
+++ src/js/main.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {
+
+	var navigation = $('nav, .blog-overlay');
+	var article = $('article');
+	var header = $('.blog-logo, .blog-open-nav');
+	var posting = $('.blog-post');
+
+	var previousPostion = 0;
+	var previousDirection = undefined;
+
+	var currentPostion = 0;
+	var currentDirection = undefined;
+
+	//스크롤링을 하면,
+	$('.blog-post').on('scroll resize', function (event) {
+		//현재 위치 구하기
+		currentPostion = posting.scrollTop();
+
+		//변화가 있는 경우에만,
+		if (previousPostion !== currentPostion) {
+			//현재 방향 구하기
+			currentDirection = previousPostion < currentPostion;
+			//포지션 업데이트
+			previousPostion = currentPostion;
+
+			//방향이 변화된 경우,
+			if (previousDirection !== currentDirection) {
+				//방향 업데이트
+				previousDirection = currentDirection;
+
+				//아래 방향 스크롤
+				if(currentDirection) {
+					header.toggleClass('is-hide', true);
+				}
+
+				//위쪽 방향 스크롤
+				else {
+					header.toggleClass('is-hide', false);
+				}
+			}
+		}
+
+		return false;
+	});
+
+	//메뉴 버튼을 누르면,
+	$('.blog-open-nav').on('click', function (event) {
+		//History API를 지원하는 경우에만,
+		if (typeof history.pushState === 'function') {
+			history.pushState('open-nav', 'nav');
+		}
+
+		navigation.toggleClass('is-visible', true);
+		article.toggleClass('scale-down', true);
+
+		return false;
+	});
+
+	//네이티브앱의 백버튼을 위해 히스토리 API 활용
+	$(window).on('popstate', function (event) {
+		//jQuery의 경우 event가 아닌 originalEvent 에 state가 저장됨
+		var status = event.state || event.originalEvent.state;
+
+		//메뉴가 오픈된 경우
+		if (status == 'open-nav') {
+			navigation.toggleClass('is-visible', true);
+			article.toggleClass('scale-down', true);
+		}
+
+		else {
+			navigation.toggleClass('is-visible', false);
+			article.toggleClass('scale-down', false);
+		}
+
+		return false;
+	});
+
+	//닫기 버튼이나 오버레이를 누르면,
+	$('.blog-close-nav, .blog-overlay').on('click', function (event) {
+		//History API를 지원하는 경우에만,
+		if (typeof history.pushState === 'function') {
+			history.back();
+		}
+
+		navigation.toggleClass('is-visible', false);
+		article.toggleClass('scale-down', false);
+
+		return false;
+	});
+
+	//검색을 시도하면,
+	$('#search').on('keypress', function (event) {
+	    //정상적으로 검색어를 입력한 경우,
+		if(event.keyCode === 13 && $(this).val().length > 0) {
+			window.location.href = "/search/" + looseURIEncode($(this).val());
+			return false;
+		}
+	});
+
+	//포스팅 삭제를 시도하면,
+	$('#delete').on('click', function (event) {
+		var url = 'http://' + window.location.hostname;
+		var post = $(this).attr("href");
+
+		if (window.confirm('이 포스트를 삭제하시겠습니까?')) {
+			$.ajax({
+				url: url + '/oldmin/entry/removeEntry' + post + '?__T__=' + Number(new Date()),
+				method: 'get',
+				headers: {
+					'X-Alt-Referer': url
+				},
+				success: function () {
+					window.location.href = url;
+				}
+			});
+		}
+
+		return false;
+	});
+
+	//이미지를 감싼 imageblock 클래스 제거
+	$('.imageblock > img').unwrap();
+
+	//prism 호환성 추가
+	$("pre[class^='brush']").each(function(a,f,d){
+		d=f.className&&f.className.substr(6);
+		d=("text"==d||"html"==d||!d)&&"markup"||"js"==d&&"javascript"||d;
+		$(f).removeClass().wrapInner('<code class="language-'+d+'"></code>')
+	});
+
+	//Google Analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-64285072-2', 'auto');ga('send', 'pageview');
+});
