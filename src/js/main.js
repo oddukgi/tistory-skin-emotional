a798c83c56fed3cbe0a7952dd177c4630ac55624
AHN <eu81273@gmail.com>
2015-08-14 17:04:39 +0900

    메뉴 버튼 변경 (작업 중)

--- src/js/main.js
+++ src/js/main.js
@@ -128,6 +128,13 @@ $(document).ready(function () {
 		$(element).removeClass().wrapInner('<code class="language-' + language + '"></code>');
 	});
 
+	//Internet Explorer 메시지
+	$('.ie-overlay').on('click', function () {
+		$(this).css('visibility', 'hidden');
+
+		return false;
+	});
+
 	//Google Analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-64285072-2', 'auto');ga('send', 'pageview');
 });
